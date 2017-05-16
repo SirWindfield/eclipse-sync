c404553962056a687ca1977e4f4525638e533cc5
SirWindfield <svenlechner@outlook.de>
2017-05-16 12:24:39 +0200

    Added fibonacci project

---
+++ Fibonacci/main.cpp
@@ -0,0 +1,21 @@
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "main.h"
+
+int main(int argc, char* argv[]) {
+    // Ausgabe rechtsseitig formatieren
+    std::cout << std::right;
+    for(int i = 0; i <= 25; i++) {
+        // formatieren
+        std::cout << "n(" << std::setfill('0') << std::setw(2) << i << "):" << std::setfill(' ') << std::setw(6);
+        // Fibonacci Ausgabe
+        std::cout << fib(i) << std::endl;
+    }
+    return 0;
+}
+
+unsigned int fib(unsigned int n) {
+    return n == 0 ? 0 : n == 1 ? 1 : fib(n - 2) + fib(n - 1);
+}
---
+++ Fibonacci/main.h
@@ -0,0 +1,6 @@
+#ifndef PRAKTIKUM_MAIN_H
+#define PRAKTIKUM_MAIN_H
+
+unsigned int fib(unsigned int);
+
+#endif PRAKTIKUM_MAIN_H
