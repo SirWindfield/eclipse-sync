5b451dbc8194635cd0d8e33b515a723c83e122ef
SirWindfield <svenlechner@outlook.de>
2017-05-26 14:52:40 +0200

    Added more stuff

--- Vektor_neu/Vektor.cpp
+++ Vektor_neu/Vektor.cpp
@@ -10,8 +10,8 @@
 #include <cmath>
 #include <iostream>
 
-
-Vektor::Vektor(double inX, double inY, double inZ) : x(inX), y(inY), z(inZ)
+Vektor::Vektor(double inX, double inY, double inZ) :
+        x(inX), y(inY), z(inZ)
 {
 
 }
@@ -21,29 +21,63 @@ Vektor::~Vektor()
 
 }
 
-double Vektor::getX() const {
+double Vektor::getX() const
+{
     return x;
 }
 
-double Vektor::getY() const {
+double Vektor::getY() const
+{
     return y;
 }
 
-double Vektor::getZ() const {
+double Vektor::getZ() const
+{
     return z;
 }
 
-double Vektor::length() const {
-    double sx = pow(getX(), 2);
-    double sy = pow(getY(), 2);
-    double sz = pow(getZ(), 2);
+Vektor Vektor::sub(const Vektor& vec) const
+{
+    return Vektor(x - vec.x, y - vec.y, z - vec.z);
+}
+
+Vektor Vektor::add(const Vektor& vec) const
+{
+    return Vektor(x + vec.x, y + vec.y, z + vec.z);
+}
+
+bool Vektor::ortho(const Vektor& vec) const
+{
+    return scalarProd(vec) == 0;
+}
+
+double Vektor::scalarProd(const Vektor& vec) const
+{
+    return x * vec.x + y * vec.y + z * vec.z;
+}
+
+double Vektor::length() const
+{
+    double sx = pow(x, 2);
+    double sy = pow(y, 2);
+    double sz = pow(z, 2);
     return sqrt(sx + sy + sz);
 }
 
+double Vektor::angle(const Vektor& vec) const
+{
+    double scalar = scalarProd(vec);
+    double rad = acos(scalar / (length() * vec.length()));
+    return rad * (180 / M_PI);
+}
+
+void Vektor::rotateAroundZ(const double rad) {
+
+}
 
 void Vektor::ausgabe() const
 {
-    std::cout << "x: " << getX() << std::endl;
-    std::cout << "y: " << getY() << std::endl;
-    std::cout << "z: " << getZ() << std::endl;
+    std::cout << "Vector[x=" << x << ", y=" << y << ", z=" << z << "]"
+            << std::endl;
+
 }
--- Vektor_neu/main.cpp
+++ Vektor_neu/main.cpp
@@ -10,11 +10,36 @@
 #include "Vektor.h"
 #include <cmath>
 
-int main()
-{ 
-   Vektor v1(0,0,0); // objeckt
-   Vektor *v = new Vektor(2,3,5); // -> pointer
+double rnd3(double value)
+{
+    return round(value * 1000.0) / 1000.0;
+}
 
-   v->ausgabe();
-   return 0;
+void test()
+{
+    Vektor v1(1, 2, 3);
+    Vektor v2(4, 5, 6);
+
+    bool result = true;
+    // test length
+    if (rnd3(v1.length()) != 3.742 || rnd3(v2.length()) != 8.775)
+    {
+        std::cout << "Length test failed" << std::endl;
+        result = false;
+    }
+
+    // test angle
+    if (rnd3(v1.angle(v2)) != 12.933)
+    {
+        std::cout << "Angle test failed" << std::endl;
+        result = false;
+    }
+
+    std::cout << (result ? "ALL TESTS PASSED" : "TESTS FAILED") << std::endl;
+}
+
+int main()
+{
+    test();
+    return 0;
 }
